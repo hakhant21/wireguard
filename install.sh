73f1afd94b0e589d7b53a4dbecea2ef43cf8aafc
Htet Aung Khant <hakhant21@gmail.com>
2026-04-17 13:41:53 +0630

    xray

--- install.sh
+++ install.sh
@@ -115,7 +115,7 @@ systemctl start wg-quick@wg0
 # ========================
 wget https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh -O xray-install.sh
 
-./xray-install.sh --install
+sudo ./xray-install.sh --install
 # Generate XRAY keys
 KEYS=$(/usr/local/bin/xray x25519)
 XRAY_PRIV=$(echo "$KEYS" | grep Private | awk '{print $3}')
